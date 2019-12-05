c201425246ce76ce86d660d743ce047e31e66f62
Wojtek <wojtex1221@gmail.com>
2019-12-05 17:31:30 +0100

    Changed FoldableMap to use HashMap with TreeSets in it.

--- src/main/maps/FoldableMap.java
+++ src/main/maps/FoldableMap.java
@@ -3,13 +3,16 @@ package maps;
 import utilities.Vector2d;
 
 import java.util.HashMap;
+import java.util.TreeSet;
 
 public class FoldableMap {
 
     private int width;
     private int height;
 
-    private HashMap<Vector2d, Object> objectsMap = new HashMap<>();
+        // na dole przechowywanie zarówno zwierząt jak i roślin
+    //komparator powinien: ustawiac w kolejności: najpierw roślina, potem najsilniejsze (pod względem energii) zwierze, ... , najsłabsze zwierze
+    private HashMap<Vector2d, TreeSet<Object>> objectsMap = new HashMap<>();
 
 
     public FoldableMap(int width, int height){
